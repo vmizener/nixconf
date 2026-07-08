5c05959ee995d43126a80fde830f80201aa03898
Raymond von Mizener <vmizener@gmail.com>
2026-07-08 18:33:56 +0000

    Add automatic garbage collection and store optimization

--- modules/common.nix
+++ modules/common.nix
@@ -11,21 +11,35 @@
       stateVersion = "25.11";
       sessionPath = ["${config.home.profileDirectory}/bin"];
     };
-    nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
+    nix = {
+      gc = {
+        automatic = true;
+        dates = "weekly";
+        options = "--delete-older-than 10d";
+      };
+      nixPath = ["nixpkgs=${inputs.nixpkgs}"];
+    };
     programs.home-manager.enable = true; # Home-Manager installs and manages itself
     systemd.user.startServices = "sd-switch"; # Reload system units on config change
     xdg.enable = true; # Enable XDG directory management
   };
-
   flake.nixosModules."common" = {...}: {
     imports = [
       self.nixosModules."common/options"
     ];
-    nix.settings.experimental-features = [
-      "flakes"
-      "nix-command"
-      "pipe-operators"
-    ];
+    nix = {
+      gc = {
+        automatic = true;
+        dates = "weekly";
+        options = "--delete-older-than 30d";
+      };
+      optimise.automatic = true;
+      settings.experimental-features = [
+        "flakes"
+        "nix-command"
+        "pipe-operators"
+      ];
+    };
     services.openssh.enable = true;
   };
 }
