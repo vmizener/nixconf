bfe6649bfb8d9a811b98f94828ea9e59d596358c
Raymond von Mizener <vmizener@gmail.com>
2026-07-24 17:49:25 -0700

    spotify

---
+++ modules/features/tools/spotify.nix
@@ -0,0 +1,18 @@
+/*
+  feat/tools/spotify
+
+Enables Spotify service.
+
+Exposes:
+
+- flake.homeModules."feat/tools/spotify":
+*/
+{
+  flake.homeModules."feat/tools/spotify" = {
+    pkgs,
+    ...
+  }: {
+    home.packages = [pkgs.spotify];
+    services.spotifyd.enable = true;
+  };
+}
