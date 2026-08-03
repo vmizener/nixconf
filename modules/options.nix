1a251b3a8b56ecd9cad7ec746ecb9d291b9c7653
Raymond von Mizener <vmizener@gmail.com>
2026-08-03 23:35:32 +0000

    formatting

--- modules/options.nix
+++ modules/options.nix
@@ -12,10 +12,17 @@
   config = {
     ################
     # Home-Manager common options
-    flake.homeModules."common/options" = {config, osConfig ? null, ...}: {
+    flake.homeModules."common/options" = {
+      config,
+      osConfig ? null,
+      ...
+    }: {
       options.flakePath = lib.mkOption {
         type = lib.types.path;
-        default = if osConfig != null then "/etc/nixos" else "${config.xdg.configHome}/home-manager";
+        default =
+          if osConfig != null
+          then "/etc/nixos"
+          else "${config.xdg.configHome}/home-manager";
         description = "Absolute path to this Nix flake (outside Nix store).  Used for out-of-store symlinks.";
       };
     };
