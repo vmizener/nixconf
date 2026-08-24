94728ecd285a637f7afe6d6459ab0ce22d976267
Raymond von Mizener <vmizener@gmail.com>
2026-08-24 19:08:11 +0000

    add zsh extraConfig option

--- modules/features/terminal/shell/zsh/default.nix
+++ modules/features/terminal/shell/zsh/default.nix
@@ -9,19 +9,31 @@ Exposes:
   - Enables Zsh for the user.
 */
 {
+  flake.homeModules."common/options" = {lib, ...}: {
+    options.features.terminal.shell.zsh = {
+      extraConfig = lib.mkOption {
+        type = lib.types.listOf lib.types.str;
+        description = "Additional config lines to append to zshrc";
+        default = [];
+      };
+    };
+  };
   flake.homeModules."feat/terminal/shell/zsh" = {
     config,
     lib,
     pkgs,
     ...
   }: let
+    cfg = config.features.terminal.shell.zsh;
+
     hmSessionVars = "${config.home.profileDirectory}/etc/profile.d/hm-session-vars.sh";
     hmNixProfile = "${config.home.profileDirectory}/etc/profile.d/nix.sh";
   in {
     programs.zsh = {
       enable = true;
       dotDir = "${config.xdg.configHome}/zsh";
-      initContent = lib.strings.concatStringsSep "\n" ([
+      initContent = lib.strings.concatStringsSep "\n" (
+        [
           ''[[ -f "${hmSessionVars}" ]] && source "${hmSessionVars}"''
           ''[[ -f "${hmNixProfile}" ]] && source "${hmNixProfile}"''
           "${builtins.readFile ./zshrc}"
@@ -30,7 +42,9 @@ Exposes:
         ++ (import ./_aliases.nix {
           inherit config pkgs;
           installed = config.home.packages;
-        }));
+        })
+        ++ cfg.extraConfig
+      );
     };
     home = {
       packages = with pkgs; [
