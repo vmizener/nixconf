573581321fbe4674c4f9e194beb3084a22dc7f3d
Raymond von Mizener <vmizener@gmail.com>
2026-08-24 19:08:22 +0000

    formatting

--- modules/features/tools/evince.nix
+++ modules/features/tools/evince.nix
@@ -8,10 +8,14 @@ Exposes:
 - flake.homeModules."feat/tools/evince":
 */
 {
-  flake.homeModules."feat/tools/evince" = {config, lib, pkgs, ...}: {
+  flake.homeModules."feat/tools/evince" = {
+    config,
+    lib,
+    pkgs,
+    ...
+  }: {
     home.packages = [pkgs.evince];
 
     features.system.mime.categories.pdfViewers = lib.mkIf config.features.system.mime.enable (lib.mkOrder 150 ["evince.desktop"]);
   };
 }
-
