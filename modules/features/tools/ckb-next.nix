251e72e959948f6ec9aab0f27d7031f2e15052f5
Raymond von Mizener <vmizener@gmail.com>
2026-09-11 16:47:32 +0000

    fix ckb-next not restarting with patched pkg; also reorganized to expose pkg similar to other non-input pkgs

--- modules/features/tools/ckb-next.nix
+++ modules/features/tools/ckb-next.nix
@@ -6,24 +6,23 @@ Enables the CKB-Next open-source driver for Corsair keyboards and mice.
 Exposes:
 
 - flake.nixosModules."feat/tools/ckb-next":
+- local package: "nix run '.#pkg:ckb-next'"
 */
-let
+{self, ...}: let
   moduleName = "feat/tools/ckb-next";
+
+  pkgName = "ckb-next";
+  # 06/02/2026 revision (supports Scimitar RGB Elite)
+  rev = "5edd3c14810c1aa93be5adde471071b97ef108b4";
+
+  localPkg = pkgs: self.packages.${pkgs.stdenv.hostPlatform.system}."pkg:${pkgName}";
 in {
   flake.nixosModules.${moduleName} = {
-    config,
     lib,
     pkgs,
     ...
   }: let
-    # 06/02/2026 revision (supports Scimitar RGB Elite)
-    src = pkgs.fetchFromGitHub {
-      owner = "ckb-next";
-      repo = "ckb-next";
-      rev = "5edd3c14810c1aa93be5adde471071b97ef108b4";
-      hash = "sha256-h5jZqiK4hYqlLFPY9jmJEniarSHE4ZcoIX4Qv3QdELc=";
-    };
-    pkg = pkgs.ckb-next.overrideAttrs (_: {inherit src;});
+    pkg = localPkg pkgs;
   in {
     mod.imported = [moduleName];
     environment.systemPackages = [pkg];
@@ -36,11 +35,22 @@ in {
       description = "Corsair keyboard next service";
       serviceConfig = {
         Type = "simple";
-        ExecStart = "${lib.getExe pkgs.ckb-next} -b";
+        ExecStart = "${lib.getExe pkg} -b";
         Restart = "on-failure";
         RestartSec = "3";
       };
       wantedBy = ["default.target"];
     };
+  };
+
+  perSystem = {pkgs, ...}: {
+    packages."pkg:${pkgName}" = pkgs.ckb-next.overrideAttrs (_: {
+      src = pkgs.fetchFromGitHub {
+        owner = "ckb-next";
+        repo = pkgName;
+        inherit rev;
+        hash = "sha256-h5jZqiK4hYqlLFPY9jmJEniarSHE4ZcoIX4Qv3QdELc=";
+      };
+    });
   };
 }
